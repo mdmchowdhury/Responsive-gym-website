a38e6b080dfe1216f34172a42a9795b45445573b
mdmchowdhury <48901710+mdmchowdhury@users.noreply.github.com>
2019-05-12 16:31:36 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($){
+
+	$(".navbar_burger").on("click",function(){
+		$("nav.nav").toggle();
+	});
+
+	$(window).resize(function(){
+		if(window.innerWidth >=920){
+			$("nav.nav").css("display","flex");
+
+		}else{
+			$("nav.nav").css("display","none");
+		}
+	})
+
+})(jQuery);
